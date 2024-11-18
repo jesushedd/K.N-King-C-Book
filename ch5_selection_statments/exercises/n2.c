1964c95854b6bfaf4e694cb937becaea2ca379fe
jesushedd <jesushe99@gmail.com>
2024-11-18 11:18:08 -0500

    Chapter 5 lecture and excercises

---
+++ ch5_selection_statments/exercises/n2.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main (void){
+    int i, j, k;
+    i = 10; j = 5;
+    printf("%d\n", !i < j);
+    // 1 true
+
+    // b)
+    i = 2, j = 1;
+    printf("%d\n", !!i + !j);
+    // 1 int
+
+    // c)
+    i = 5; j = 0; k = -5;
+}
